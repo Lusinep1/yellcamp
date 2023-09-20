bdd7824c2671694686defcbb973a6569b1c307f9
Lusine Papanyan <lpapanyan9611@gmail.com>
2023-09-21 00:39:43 +0400

    add partials and layout

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000;
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 const Campground = require("./models/campground");
 const mongoose = require("mongoose");
 
@@ -17,6 +18,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
