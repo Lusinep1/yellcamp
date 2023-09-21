e9e419cb382e804eb5984b5f0e7d5caeceefc0d0
Lusine Papanyan <lpapanyan9611@gmail.com>
2023-09-21 13:36:03 +0400

    basic styles

--- seeds/index.js
+++ seeds/index.js
@@ -19,9 +19,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const priceTag = Math.floor(Math.random() * 100) + 15;
     const s = new Campground({
       title: `${sample(descriptors)} ${sample(places)}`,
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      image: "https://source.unsplash.com/collection/483251",
+      description:
+        "Lorem ipsum dolor sit, aunt multum legendum isse, non multa. Aqua vitae. Docendo discimus",
+      price: priceTag,
     });
     await s.save();
   }
