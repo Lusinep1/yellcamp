b737b8a3d331118bf7680db51a5c6fefb5260a62
Lusine Papanyan <lpapanyan9611@gmail.com>
2023-10-12 13:20:48 +0400

    add registration post route

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
+const catchAsync = require("../utils/catchAsync");
 
 router.get("/register", (req, res) => {
   res.render("users/register");
 });
 
-router.post("/register", async (req, res) => {
-  res.send(req.body);
-});
+router.post(
+  "/register",
+  catchAsync(async (req, res) => {
+    try {
+      const { email, username, password } = req.body;
+      const user = new User({ email, username });
+      const registeredUser = await User.register(user, password);
+      req.flash("success", "Welcome to YellCamp");
+      res.redirect("/campgrounds");
+    } catch (error) {
+      req.flash("error", error.message);
+      res.redirect("/register");
+    }
+  })
+);
 
 module.exports = router;
