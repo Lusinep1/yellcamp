681869453f7181cff22ea602267c5aa43e68bbd4
Lusine Papanyan <lpapanyan9611@gmail.com>
2023-10-12 15:48:31 +0400

    add logout

--- routes/users.js
+++ routes/users.js
@@ -42,4 +42,15 @@ router.post(
   }
 );
 
+//logout
+router.get("/logout", (req, res, next) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    req.flash("success", "Goodbye!");
+    res.redirect("/campgrounds");
+  });
+});
+
 module.exports = router;
