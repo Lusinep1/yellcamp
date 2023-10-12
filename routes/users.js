7fba0048523c0b38d2a5a53b50043f81529e8490
Lusine Papanyan <lpapanyan9611@gmail.com>
2023-10-12 16:40:15 +0400

    add register that loggs you in

--- routes/users.js
+++ routes/users.js
@@ -4,20 +4,23 @@ const User = require("../models/user");
 const catchAsync = require("../utils/catchAsync");
 const passport = require("passport");
 
-//register
+//register route
 router.get("/register", (req, res) => {
   res.render("users/register");
 });
 
 router.post(
   "/register",
-  catchAsync(async (req, res) => {
+  catchAsync(async (req, res, next) => {
     try {
       const { email, username, password } = req.body;
       const user = new User({ email, username });
       const registeredUser = await User.register(user, password);
-      req.flash("success", "Welcome to YellCamp");
-      res.redirect("/campgrounds");
+      req.login(registeredUser, (err) => {
+        if (err) return next(err);
+        req.flash("success", "Welcome to YellCamp");
+        res.redirect("/campgrounds");
+      });
     } catch (error) {
       req.flash("error", error.message);
       res.redirect("/register");
