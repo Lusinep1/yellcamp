acfa34f9d1ad53562d591bb8b4a15c4e5b00bb4e
Lusine Papanyan <lpapanyan9611@gmail.com>
2023-10-16 16:33:24 +0400

    add image and ket stuff

--- routes/campground.js
+++ routes/campground.js
@@ -4,16 +4,22 @@ const catchAsync = require("../utils/catchAsync");
 const Campground = require("../models/campground");
 const { isLoggedIn, validateCampground, isAuthor } = require("../middleware");
 const campgrounds = require("../controllers/campgrounds");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 // slash routes
 router
   .route("/")
   .get(catchAsync(campgrounds.index))
-  .post(
-    isLoggedIn,
-    validateCampground,
-    catchAsync(campgrounds.createCampground)
-  );
+  // .post(
+  //   isLoggedIn,
+  //   validateCampground,
+  //   catchAsync(campgrounds.createCampground)
+  // );
+  .post(upload.array("image"), (req, res) => {
+    console.log(req.body, req.files);
+    res.send("smth");
+  });
 
 //New
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
