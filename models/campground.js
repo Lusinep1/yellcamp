3a6ecc1147a2cbd61fc673634da1c1ff210cc3ea
Lusine Papanyan <lpapanyan9611@gmail.com>
2023-10-17 17:47:23 +0400

    add thumbnail api from cloudinary

--- models/campground.js
+++ models/campground.js
@@ -2,9 +2,21 @@ const mongoose = require("mongoose");
 const Review = require("./review");
 const Schema = mongoose.Schema;
 
+//  https://res.cloudinary.com/diwi3pixz/image/upload/w_100/v1697483604/yellcamp/cruraq7wueactbpyycox.jpg
+//c_crop,g_face,h_400,w_400/r_max/c_scale,w_200/f_auto/lady.png
+
+const ImageSchema = new Schema({
+  url: String,
+  filename: String,
+});
+
+ImageSchema.virtual("thumbnail").get(function () {
+  return this.url.replace("/upload", "/upload/w_200");
+});
+
 const CampgroundScema = new Schema({
   title: String,
-  images: [{ url: String, filename: String }],
+  images: [ImageSchema],
   price: Number,
   description: String,
   location: String,
